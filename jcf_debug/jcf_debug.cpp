2acea31237b4b638d677fc2ff1152875d00005ec
chrhaase <chrhaase@outlook.dk>
2022-09-07 12:00:37 +0200

    Fix and suppress warnings

--- jcf_debug/jcf_debug.cpp
+++ jcf_debug/jcf_debug.cpp
@@ -2,6 +2,10 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wsign-conversion"
+#pragma clang diagnostic ignored "-Winconsistent-missing-destructor-override"
+#pragma clang diagnostic ignored "-Wfloat-conversion"
+#pragma clang diagnostic ignored "-Wshadow-field-in-constructor"
 
 #include "jcf_debug.h"
 
